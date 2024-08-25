349a25cfa450b4cd97f4399ff42f98a4f81a47db
Deep <deepgoyani77@gmail.com>
2024-08-25 15:55:31 +0530

    JavaScipt

---
+++ index.js
@@ -0,0 +1,428 @@
+// // console.log('Deep')
+// // var nme ='deep'
+
+// // for (i=0;i<=10;i++){
+// //     console.log(i)
+// // }
+// // var m=3
+// // m=9
+// // for (i=0;i<=10;i++){
+// //     console.log(i*m)
+// // }
+// // var new_object={new:'value'}
+// // console.log('new_object')
+// // var array=[1,2,3,4,5,6]
+// // console.log(array)
+// // function for_loop(){
+// //     for(i=0;i<=10;i++){
+// //         console.log(i)
+// //     }}
+// //     for_loop()
+// //   var i=[1,2,3,4,5,6]
+// //   console.log(i.at(1))  
+// //   i.join('*')
+// //   console.log(i)
+
+
+
+
+// //   function isArmstrongNumber(num) {
+// //     // Convert the number to a string to easily iterate over digits
+// //     const numStr = num.toString();
+// //     const numDigits = numStr.length;
+    
+// //     // Calculate the sum of each digit raised to the power of the number of digits
+// //     const sum = numStr.split('').reduce((accumulator, digit) => {
+// //         return accumulator + Math.pow(parseInt(digit), numDigits);
+// //     }, 0);
+
+// //     // Check if the sum is equal to the original number
+// //     return sum === num;
+// // }
+
+// // // Example usage:
+// // const number = 153;
+// // if (isArmstrongNumber(number)) {
+// //     console.log(`${number} is an Armstrong number.`);
+// // } else {
+// //     console.log(`${number} is not an Armstrong number.`);
+// // }
+// // var sum=0;
+// // var number="153";
+// // function isArmstrongNumber(number){
+  
+// //   m=number.length;
+// //   for (i=0;i<m;i++){
+// //     sum=sum+(number[i]**m);
+// //   }
+// //   if (sum==number){
+// //     console.log('number is armstrong')
+// //   }
+// //   else{
+// //     console.log('number is not armstrong')
+// //   }
+  
+  
+// // }
+
+
+// // isArmstrongNumber(number)
+
+
+// // console.log("Hello World")
+// // var name =['Deep']
+// // var Adress =['vapi']
+// // var city =['vapi']
+// // console.log(name)
+// // console.log(Adress)
+// // console.log(city)
+// // r=14
+// // area =3.14*r
+// // console.log(area)
+
+// // x=2
+// // y=3
+// // console.log(x+y)
+// // console.log(x*y)
+// // console.log(x/y)
+// // var temp;
+// // temp=x;
+// // x=y;
+// // y=temp;
+// // console.log(x)
+// // console.log(y)
+// // n=3
+// // m=5
+// // if (n<m){
+// //   console.log('n is smaller')
+// // }
+// // else{
+// //   console.log('m is greater than n ')
+// // }
+// // if (n>m){
+// //   console.log('n is greater')
+// // }
+// // else{
+// //   console.log('m is greater')
+// // }
+// // n=3
+// // m=4
+// // z=9
+// // if (z<n>m){
+// //   console.log("n is greater")
+// // }
+// // else if (z<n<m){
+// //   console.log('n is greater than z but less than m')
+// // }
+// // else if (z>n>m){
+// //   console.log('z is greater than n and n is greater than m')
+// // }
+// // else {
+// //   console.log('z is greater than n and m ')
+// // }
+// // n=1
+// // for (i=1;i<=10;i++){
+// //   console.log(n*i)
+
+// // }
+// // n=2
+// // for (i=1;i<=10;i++){
+// //   console.log(n*i)
+
+// // }
+
+// // n=3
+// // for (i=1;i<=10;i++){
+// //   console.log(n*i)
+
+// // }
+// // n=4
+// // for (i=1;i<=10;i++){
+// //   console.log(n*i)
+
+// // }
+// // n=5
+// // for (i=1;i<=10;i++){
+// //   console.log(n*i)
+
+// // }
+// // n=6
+// // for (i=1;i<=10;i++){
+// //   console.log(n*i)
+
+// // }
+// // n=7
+// // for (i=1;i<=10;i++){
+// //   console.log(n*i)
+
+// // }
+// // n=8
+// // for (i=1;i<=10;i++){
+// //   console.log(n*i)
+
+// // }
+// // n=9
+// // for (i=1;i<=10;i++){
+// //   console.log(n*i)
+
+// // }
+// // n=10
+// // for (i=1;i<=10;i++){
+// //   console.log(n*i)
+
+// // }
+// // n=11
+// // for (i=1;i<=10;i++){
+// //   console.log(n*i)
+
+// // }
+// // n=12
+// // for (i=1;i<=10;i++){
+// //   console.log(n*i)
+
+// // }
+// // sum45=0
+// // for (i=45;i<=65;i++){
+ 
+// //   sum45 =sum45 + i
+// // }
+// // console.log(sum45)
+// // var number=7;
+// // var count=0;
+
+// // for(var i=2; i<number;i++){
+// //     if(number%i==0){
+// //         count=count+1
+// //     }
+// // }
+// // if (count>0){
+// //     console.log(number, "is not a prime number")
+    
+// // }
+// // else{
+// //     console.log(number, "is  prime number")
+// // }
+// // console.log('********')
+// // rarray=[];
+// // for (i=101;i<=130;i++){
+// //   rarray.push(i)
+// // }
+// // console.log(rarray)
+// // b=('my name')
+// // d=('is deep')
+// // console.log(b.concat(d))
+
+     
+// // b=console.log(b)
+// // no1=2;
+// // no2=3;
+// // [no1,no2]=[no2,no1]
+// // console.log('no1',no1)
+// // console.log('no2',no2)
+
+// // var number = 223;
+// // var sum = 0;
+
+// // while (number > 0) {
+// //   sum = sum + (number % 10); 
+// //   number = (number -(number % 10))/10; 
+// // }
+
+// // console.log(sum);
+
+// const colors = ["red", "green", "blue"];
+// const [firstColor, secondColor, thirdColor] = colors;
+
+// console.log(firstColor); 
+// console.log(secondColor); 
+// console.log(thirdColor);
+
+// const cars=['lambo','merc','buggati','dodge','mclaren']
+// const [car1,,,,car5]=cars
+// console.log(car1)
+// console.log(car5)
+
+// let ecar={bname:'bj',age:'infinite'}
+// let{age,bname}={bname:'bj',age:'infinite'}
+// console.log(bname)
+
+// const uniquenumbers= new Set();
+// uniquenumbers.add(1)
+// console.log("set:Unique numbers-",Array.from(uniquenumbers).join(","))
+
+
+
+// let arr='aa,bbb,cc,dddd';
+// var alp= arr.split(",");
+// console.log(alp)
+// console.log(alp.length)
+// let max=alp[0].length
+// max_word=''
+// for(i=1;i<alp.length;i++){ 
+//     if(max>alp[i].length){
+//         max_word=max
+//     }
+//     else{
+//        max_word=alp[i].length
+//     }
+//     }
+//     console.log(max_word)
+
+a='hello,world';
+b=a.split(',');
+console.log(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
